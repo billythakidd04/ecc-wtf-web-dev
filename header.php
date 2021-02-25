c5cedafd4923901342896f1aa10bb3c3e27bbd40
Bill Caffery <billythakidd04@gmail.com>
2021-02-25 13:28:29 -0500

    added the form fields to the correct file and updated header

--- header.php
+++ header.php
@@ -4,4 +4,5 @@
             <li><a href="../Week_2_css_and_intro_to_php/html_test.php">Week 2 - intro to PHP</a></li>
             <li><a href="../Week_3_github_and_cli/html_test.php">Week 3 - CLI and Github</a></li>
             <li><a href="../Week_4_Advanced_CSS/html_test.php">Week 4 - Advanced CSS</a></li>
+            <li><a href="../Week_5_Databases/html_test.php">Week 5 - Databases</a></li>
         </ul>
