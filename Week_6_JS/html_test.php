8210d2ba51919227fc7fe92f60786e37fcac7763
Bill Caffery <billythakidd04@gmail.com>
2021-03-10 15:28:27 -0500

    fixed double semi colon everywhere and moved alert to bottom of file

--- Week_6_JS/html_test.php
+++ Week_6_JS/html_test.php
@@ -8,7 +8,6 @@ ini_set('display_errors', 1);
 	<title>
 		this is a title
 	</title>
-	<script src="js/main.js"></script>
 	<!-- bootstrap css not used atm-->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	<!-- site css -->
@@ -267,6 +266,8 @@ ini_set('display_errors', 1);
 	<footer></footer>
 	<!-- bootstrap js load last -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+	<script src="js/main.js"></script>
 </body>
 
 </html>
