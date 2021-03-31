1ce0b099e98de627b996fc27d1bb61a8f42c3dbb
Bill Caffery <billythakidd04@gmail.com>
2021-03-31 17:44:34 -0400

    'fix' error on form save

--- Week_9_PHP_APIs/src/Group.php
+++ Week_9_PHP_APIs/src/Group.php
@@ -118,7 +118,7 @@ class Group
 		return $retGroup;
 	}
 
-	public function findGroupByNumber(): Group
+	public function findGroupByNumber()
 	{
 		$dbCon = $this->db->getConnection();
 		$sql = "SELECT * FROM `Groups` WHERE groupNumber = ".$dbCon->real_escape_string($this->getGroupNumber());
