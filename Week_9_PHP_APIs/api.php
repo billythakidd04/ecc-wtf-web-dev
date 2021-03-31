0adc8d050360d4a890b74223654d332c263a13d3
Bill Caffery <billythakidd04@gmail.com>
2021-03-31 03:30:18 -0400

    slim works... don't ask how... It's 3am and i just dont care

--- Week_9_PHP_APIs/api.php
+++ Week_9_PHP_APIs/api.php
@@ -10,38 +10,38 @@ use \WFDWeb\Student;
 
 $app = AppFactory::create();
 
-$middleware = $app->addErrorMiddleware(true,true,true);
+// adding this so I dont have to do some tricky nonsense and can avoid long route names... efficient huh? 
+$app->setBasePath((function () {
+	// literally everything in here is to avoid typing Week_whatever each time this gets copied for the next week. DO NOT DO THIS!!!
+	$scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+	$uri = (string) parse_url('http://a' . $_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
+	if (stripos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
+		return $_SERVER['SCRIPT_NAME'];
+	}
+	if ($scriptDir !== '/' && stripos($uri, $scriptDir) === 0) {
+		return $scriptDir;
+	}
+	return '';
+})().'/api');// Append route with api cuz I don't want to write that every time also
 
-// $app->setBasePath((function () {
-// 	$scriptDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
-// 	$uri = (string) parse_url('http://a' . $_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
-// 	if (stripos($uri, $_SERVER['SCRIPT_NAME']) === 0) {
-// 		return $_SERVER['SCRIPT_NAME'];
-// 	}
-// 	if ($scriptDir !== '/' && stripos($uri, $scriptDir) === 0) {
-// 		return $scriptDir;
-// 	}
-// 	return '';
-// })());
-
+// this route does nothing but print "test" I'm leaving it here for testing
 $app->get('/', function (Request $request, Response $response, array $args) {
-	// $name = $args['name'];
 	$response->getBody()->write("test");
 
 	return $response;
 });
 
-// handle show all students route `/api/students/`
-$app->get('/api/students/', function (Request $request, Response $response, array $args) {
+// handle show all students route `/students/`
+$app->get('/students/', function (Request $request, Response $response, array $args) {
 	echo 'show all students';
-	// $students = Student::listStudents();
-	// $studentsJSON = json_encode($students);
-	$response->getBody()->write('test');
+	$students = Student::listStudents();
+	$studentsJSON = json_encode($students);
+	$response->getBody()->write($studentsJSON);
 
 	return $response;
 });
 
-$app->get('/api/hello/{name}', function (Request $request, Response $response, array $args) {
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
 	$name = $args['name'];
 	$response->getBody()->write("Hello, $name");
 
